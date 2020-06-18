0a3ee1ef2fdd96257521d09af009356b856073b8
Tingwei-hub <64124325+Tingwei-hub@users.noreply.github.com>
2020-06-18 11:25:30 +0800

    Updated game by Lau Ting Wei

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class GameManager : MonoBehaviour
     {
         if (Time.timeScale == 0 && Input.GetKeyDown(KeyCode.Return))
             StartGame();
+        
     }
 
     public void UpdateScore(int value)
@@ -41,5 +43,9 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0;
         Txt_Message.text = "GAMEOVER! \nPRESS ENTER TO RESTART GAME.";
         Txt_Message.color = Color.red;
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    public Vector3 jump;
+    public float velocity = 2.0f;
+    Rigidbody rb;
+    
+    
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = (Vector3.up * velocity);
+        }
+
+        transform.position = new Vector3(0, Mathf.Clamp(transform.position.y, -5000f, 3.6f), 0);
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            SceneManager.LoadScene("gameOver");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Collider")
+        {
+            
+            GameManager.thisManager.UpdateScore(1);
+        }
     }
 }
